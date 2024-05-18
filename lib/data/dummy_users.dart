bf9187a58231d7d98f414a6ab3106b03b05f2c6d
Mauricio-Lourenco <junior_301@live.com>
2024-05-18 14:52:35 -0300

    Init Cadastro

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+      id: '1',
+      name: 'Mário',
+      email: 'mario@user.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2023/12/06/22/21/ai-generated-8434461_1280.jpg'),
+  '2': const User(
+      id: '2',
+      name: 'Pingo',
+      email: 'pingo@user.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2024/04/09/19/30/ai-generated-8686666_1280.jpg'),
+  '3': const User(
+      id: '3',
+      name: 'Eagle',
+      email: 'eagle@user.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2024/04/17/13/53/ai-generated-8702135_1280.png'),
+  '4': const User(
+      id: '4', name: 'Random', email: 'random@user.com', avatarUrl: ''),
+};
